2e522b9d937497a661179b354b5fbad6ca395688
ljhee3611 <ljhee3611@gmail.com>
2026-02-04 03:51:52 +0000

    feat: Create lottery number generator

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Add JS here
+// Function to generate 6 unique random numbers between 1 and 45
+export function generateLottoNumbers() {
+  const numbers = new Set();
+  while (numbers.size < 6) {
+    const randomNumber = Math.floor(Math.random() * 45) + 1;
+    numbers.add(randomNumber);
+  }
+  return Array.from(numbers).sort((a, b) => a - b);
+}
