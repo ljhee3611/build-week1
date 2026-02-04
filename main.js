eee698520a154d7ca98e2e49163f67833a653e61
ljhee3611 <ljhee3611@gmail.com>
2026-02-04 03:57:15 +0000

    feat: Allow user to choose number of balls

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-// Function to generate 6 unique random numbers between 1 and 45
-export function generateLottoNumbers() {
+// Function to generate unique random numbers between 1 and 45
+export function generateLottoNumbers(count = 6) {
   const numbers = new Set();
-  while (numbers.size < 6) {
+  while (numbers.size < count) {
     const randomNumber = Math.floor(Math.random() * 45) + 1;
     numbers.add(randomNumber);
   }
